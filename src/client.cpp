8b16efd8ba3cb8f517163fbb1ab157da7fd6daf7
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-09-08 18:44:37 +0530

    created client server communication

--- src/client.cpp
+++ src/client.cpp
@@ -1,38 +1,106 @@
-#include<iostream> 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+using namespace std;
 
-using namespace std; 
+void *recieve_message(void *socket_ptr);
+void send_message(int client_socket);
+void welcome_message();
 
+int main()
+{
+    welcome_message();
 
-void setup(){
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
-    sockaddr_in serverAddr; 
-    serverAddr.sin_family = AF_INET; 
-    serverAddr.sin_port = htons(8080); 
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (client_socket == -1)
+    {
+        cerr << "Socket Creation Failed";
+        return -1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(client_socket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        cerr << "Connection To server Failed" << endl;
+        close(client_socket);
+        return -1;
+    }
+    cout << "Connected To the server" << endl;
+
+    pthread_t thread_id;
+
+    pthread_create(&thread_id, nullptr, recieve_message, (void *)&client_socket);
+
+    cout << "ENTER YOUR MESSAGE TO SEND TO SERVER " << endl;
+
+    send_message(client_socket);
+    close(client_socket);
+    return 0;
 }
 
-int main(){
-    
+void *recieve_message(void *socket_ptr)
+{
 
+    int client_socket = *(int *)socket_ptr;
+    char buffer[1024];
 
-    
-    connect(
-        clientSocket, 
-        (struct sockaddr*)&serverAddr,
-        sizeof(serverAddr)
-    ); 
-    
+    while (1)
+    {
+        memset(buffer, 0, sizeof(buffer));
 
-    const char* message = "Hello Server"; 
+        int bytesRecv = recv(client_socket, buffer, sizeof(buffer), 0);
 
-    send(clientSocket, message, strlen(message), 0);
+        if (bytesRecv <= 0)
+        {
 
-    return 0; 
+            cerr << "Connection Closed or Error Occured" << endl;
+            close(client_socket);
+
+            pthread_exit(nullptr);
+        }
+
+        cout << "Server : " << buffer << endl;
+    }
+
+    return nullptr;
+}
+
+void send_message(int client_socket)
+{
+    char buffer[1024];
+    while (1)
+    {
+        cin.getline(buffer, sizeof(buffer));
+
+        if (send(client_socket, buffer, strlen(buffer), 0) == -1)
+        {
+            cerr << "Failed To send message " << endl;
+            break;
+        }
+    }
+}
+
+void welcome_message()
+{
+
+    cout << R"(
+    ███████╗██╗  ██╗██╗██████╗ ██╗██████╗ ██╗     ██████╗██╗  ██╗ █████╗ ████████╗
+    ██╔════╝██║ ██╔╝██║██╔══██╗██║██╔══██╗██║    ██╔════╝██║  ██║██╔══██╗╚══██╔══╝
+    ███████╗█████╔╝ ██║██████╔╝██║██║  ██║██║    ██║     ███████║███████║   ██║   
+    ╚════██║██╔═██╗ ██║██╔══██╗██║██║  ██║██║    ██║     ██╔══██║██╔══██║   ██║   
+    ███████║██║  ██╗██║██████╔╝██║██████╔╝██║    ╚██████╗██║  ██║██║  ██║   ██║   
+    ╚══════╝╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝╚═════╝ ╚═╝     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝                                                                    
+                                                                                
+)" << endl;
 }
